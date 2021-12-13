018178428bd519aadeda9c862455a8f34c6faa8a
David Chalifoux <dccfoux@gmail.com>
2021-12-13 12:41:17 -0500

    Add robots.txt

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/robots'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
