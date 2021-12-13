d1bf9552cbf1cc2a7bea1504b0dde1bef20a3e3b
David Chalifoux <dccfoux@gmail.com>
2021-12-13 11:39:41 -0500

    Update SEO tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,19 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Colossal Cave Adventure',
+    title: 'Colossal Cave Adventure Online',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Play the original Colossal Cave text adventure game in your browser! You can also use voice commands and text-to-speech.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
